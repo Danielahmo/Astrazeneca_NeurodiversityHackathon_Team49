d88429d7774e1d3b94121f5442cbec3f7927b8f5
Danielahmo <77475263+Danielahmo@users.noreply.github.com>
2021-03-27 05:42:10 -0600

    Delete Hola.m

--- Interface/Hola.m
+++
@@ -1,6 +0,0 @@
-# Graphic Interface: Simulation of Hardware and Display of Eye-Tracking Algoritm
-
-This interface was coded with Matlab App Designer. The zip file team049_interface contains the .mlapp file (Design and Code), as well as the required
-photos and videos for the Design View and for the Eye-Tracking Display. In the zipe file, a .mlappinstall file is also included. This file allows the 
-user to test out the inputs of the simulated fidgeting device after 'turning it on' and the correspondent alerts that this'll generate as a Matlab App. 
-An exemplary processed video for the eye-tracking algorithm is also displayed after pressing the "Video" button. 
